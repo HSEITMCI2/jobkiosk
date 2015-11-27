35da3ff1ac6476306c3721b72dfef42a9dcaf9f6
Eugen Muschewski <e.muschewski@gmail.com>
2015-11-27 15:17:59 +0100

    basic javascript for job offers

--- source/public/js/joboffer.js
+++ source/public/js/joboffer.js
@@ -8,27 +8,47 @@
 
 // 	var fields = ['familyname', 'givenname', 'email', 'company', 'department', 'street', 'postcode', 'city', 'countrytxt', 'password', 'resetPasswordToken', 'resetPasswordExpires'];
 
+// pseudo database
+var mockdata = [{ jobTitle: 'myfirsttitle' }, { jobTitle: 'mysecondtitle' }];
+
+
 "use strict";
 
 function InitJoboffer() {
 
 
     var that = {};
-    that.setup = function () {
+    that.setup = function (jobData) {
+        for (var i = 0; i < jobData.length; i++) {
 
-        var input = dom('input', {
+            var job = jobData[i];
 
-            name: jobTitle,
-            class: 'jobInput'
-        }, '');
-        // greife auf jade / html id jobcontent zu
-        var jobContent = document.getElementById("jobcontent");
-        jobContent.appendChild(input);
+            // create fields with dom
+            var input = dom('input', {name: 'jobTitle',class: 'jobInput'}, '');
+            var changeBtn = dom('button', {}, 'change');
+            var deleteBtn = dom('button', {}, 'delete');
 
+            // put everything into div
+            var jobContainer = dom('div', {}, input, changeBtn, deleteBtn);
+
+            // delete job function
+            deleteBtn.jobIndex = i;
+            deleteBtn.addEventListener('click', function (e) {
+                console.log(e.target.jobIndex);
+                jobData.splice(e.target.jobIndex, 1);
+                removeChildren(jobContent);
+                that.setup(jobData);
+            });
+            
+            // access jade / html id jobcontent
+            input.value = job.jobTitle;
+            var jobContent = document.getElementById("jobcontent");
+            jobContent.appendChild(jobContainer);
+        }
     }
     return that;
 }
 
 window.addEventListener('load', function () {
-    InitJoboffer().setup();
+    InitJoboffer().setup(mockdata);
 });
