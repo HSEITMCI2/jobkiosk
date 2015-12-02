0fbc324f6a208b7c7426af4502506f9b6187c34e
Andreas Roessler, HSE <andreas.roessler@hs-esslingen.de>
2015-12-02 08:12:10 +0100

    prepare test jobs

---
+++ source/spec/test_jobs.js
@@ -0,0 +1,40 @@
+
+var fs = require('fs');
+
+GLOBAL.searchpaths(module);
+var log = require('log');
+
+var moduleName = "testjobs]:";
+var errorLog = log.xlog("[Error in " + moduleName, "FgWhite", "BgRed", 0);
+// var warningLog = log.xlog("[Warning " + moduleName, "FgRed", "BgWhite", 1);
+// var infoLog = log.xlog("[Info in " + moduleName, "FgGreen", "BgBlack", 2);
+var dbgLog = log.xlog("[Debug " + moduleName, "FgYellow", "BgGreen", 3);
+
+
+function CreateJobs() {
+	"use strict";
+	var that = {};
+
+
+	that.createPDF = function(filename) {
+
+	}
+
+	return that;
+}
+
+
+function TestJobs() {
+	"use strict";
+	var singleton = null;
+
+	return function() {
+		if (singleton === null) {
+			singleton = CreateJobs();
+		}
+		return singleton;
+	};
+}
+
+
+module.exports = TestJobs();
