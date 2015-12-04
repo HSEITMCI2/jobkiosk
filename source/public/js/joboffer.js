977eb92b405acd752096245bc666f9f02d1dadb1
David Mändlen <david.maendlen@bifroe.st>
2015-12-04 09:30:49 +0100

    implemented minor stuff in js

--- source/public/js/joboffer.js
+++ source/public/js/joboffer.js
@@ -17,38 +17,38 @@ var mockdata = [{ jobTitle: 'myfirsttitle' }, { jobTitle: 'mysecondtitle' }];
 function InitJoboffer() {
 
 
-    var that = {};
-    that.setup = function (jobData) {
-        for (var i = 0; i < jobData.length; i++) {
+	var that = {};
+	that.setup = function (jobData) {
+		for (var i = 0; i < jobData.length; i++) {
 
-            var job = jobData[i];
+			var job = jobData[i];
 
-            // create fields with dom
-            var input = dom('input', {name: 'jobTitle',class: 'jobInput'}, '');
-            var changeBtn = dom('button', {}, 'change');
-            var deleteBtn = dom('button', {}, 'delete');
+			// create fields with dom
+			var input = dom('input', {name: 'jobTitle',class: 'jobInput'}, '');
+			var changeBtn = dom('button', {}, 'change');
+			var deleteBtn = dom('button', {}, 'delete');
 
-            // put everything into div
-            var jobContainer = dom('div', {}, input, changeBtn, deleteBtn);
+			// put everything into div
+			var jobContainer = dom('div', {}, input, changeBtn, deleteBtn);
 
-            // delete job function
-            deleteBtn.jobIndex = i;
-            deleteBtn.addEventListener('click', function (e) {
-                console.log(e.target.jobIndex);
-                jobData.splice(e.target.jobIndex, 1);
-                removeChildren(jobContent);
-                that.setup(jobData);
-            });
-            
-            // access jade / html id jobcontent
-            input.value = job.jobTitle;
-            var jobContent = document.getElementById("jobcontent");
-            jobContent.appendChild(jobContainer);
-        }
-    }
-    return that;
+			// delete job function
+			deleteBtn.jobIndex = i;
+			deleteBtn.addEventListener('click', function (e) {
+				console.log(e.target.jobIndex);
+				jobData.splice(e.target.jobIndex, 1);
+				removeChildren(jobContent);
+				that.setup(jobData);
+			});
+
+			// access jade / html id jobcontent
+			input.value = job.jobTitle;
+			var jobContent = document.getElementById("jobcontent");
+			jobContent.appendChild(jobContainer);
+		}
+	}
+	return that;
 }
 
 window.addEventListener('load', function () {
-    InitJoboffer().setup(mockdata);
+	InitJoboffer().setup(mockdata);
 });
