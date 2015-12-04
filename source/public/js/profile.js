4874c6c36663c1f1589025f0cbfdd154cfa1590e
Eugen Muschewski <e.muschewski@gmail.com>
2015-12-04 16:51:24 +0100

    worked on the profile

--- source/public/js/profile.js
+++ source/public/js/profile.js
@@ -18,19 +18,41 @@ function InitProfile() {
 		http('get', '/api/profile');
 
 		http('post', '/api/profile', {
-			'familyname': 'Mustermann',
-			'givenname': 'Sexy Anna',
+			'familyname': 'Anna',
+			'givenname': 'Sexy',
 			'company': 'KILL JAVASCRIPT AG',
 			'department': 'WC',
-			'telephone' : '0800123456',
+			'telephone' : 'its a phone nr im serius',
 			'street': 'langestr',
 			'postcode': '666',
 			'city': 'hell',
 			'countrytxt' :'ka'
 
+		});
+
+		var changeBtn = dom('button', { class: 'jobdeletebtn' }, 'SUBMIT');
+
+		var jobContainer = dom('div', { class: 'rowborder', style: 'justify-content:center;' }, changeBtn);
+
+		// delete job function
+		changeBtn.addEventListener('click', function (e) {
+			console.log(e.target.jobIndex);
+
+			var r = confirm("Are you sure?!");
+			if (r == true) {
+				// do nothing
+			} else {
+				// do nothing
+			}
 
 		});
+
+		var jobContent = document.getElementById("submitthis");
+		jobContent.appendChild(jobContainer);
+
 	};
+
+
 
 	return that;
 }
