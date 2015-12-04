74a8c7f3b5e8964a19ed1f6cea9f430ce6d9e9fd
David Mändlen <david.maendlen@bifroe.st>
2015-12-04 10:18:53 +0100

    activating new job offer

--- source/server_modules/user_routes.js
+++ source/server_modules/user_routes.js
@@ -22,6 +22,7 @@ var insideLinks = {
 	'/logout': 'Logout',
 	'/profile': 'Profile',
 	'/jobs': 'Jobs',
+	'/newJobOffer': 'new job offer',
 	'/companyoffer': 'Company Offer',
 	'/SpecRunner.html': 'Test'
 };
@@ -89,6 +90,12 @@ module.exports = function(app, passport) {
 	app.get('/companyoffer', isLoggedIn, function (req, res) {
 	    para.user = req.user;
 	    res.render('companyoffer', para);
+	});
+
+	// New Job offer section =================
+	app.get('/newJobOffer', isLoggedIn, function(req, res){
+		para.user = req.user;
+		res.render('newjoboffer', para);
 	});
 
 
