37f49c690038401469058ae2750a40cad7fa0683
Andreas Roessler, HSE <andreas.roessler@hs-esslingen.de>
2015-12-05 11:07:28 +0100

    jshint works only on changed files - not on all

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -1,11 +1,11 @@
-
-(function () {
+(function() {
 	'use strict';
 	// grab our packages
-	var gulp   = require('gulp');
+	var gulp = require('gulp');
 	var debug = require('gulp-debug');
 	var jshint = require('gulp-jshint');
 	var jasmine = require('gulp-jasmine');
+	var cache = require('gulp-cached');
 
 	var monitor = require('./gulp_modules/monitor');
 
@@ -18,39 +18,47 @@
 
 	var jsdirs = ['*.js', 'server_modules/*.js', 'public/js/*.js', 'server_modules/routes/*.js'];
 	var serverfiles = ['app*.js', 'server_modules/*.js', 'server_modules/routes/*.js'];
-	var clientfiles = ['public/**/*', 'spec/*.html', 'spec/*.js',  'views/*.jade'];
+	var clientfiles = ['public/**/*', 'spec/*.html', 'spec/*.js', 'views/*.jade'];
+
 
 	// configure the jshint task
 	gulp.task('jshint', function() {
-		return gulp.src( jsdirs )
-		.pipe(debug({title: 'js:'}))
-		.pipe(jshint())
-		.pipe(jshint.reporter('jshint-stylish'));
+		return gulp.src(jsdirs)
+			.pipe(cache('linting'))
+			.pipe(debug({
+				title: 'js:'
+			}))
+			.pipe(jshint())
+			.pipe(jshint.reporter('jshint-stylish'));
 	});
 
 	gulp.task('servermonitor', function() {
-		gulp.watch(serverfiles, monitor.watch4Restart);		
+		gulp.watch(serverfiles, monitor.watch4Restart);
 	});
 
 	gulp.task('browsermonitor', function() {
-		gulp.watch(clientfiles, monitor.watch4Reload);		
+		gulp.watch(clientfiles, monitor.watch4Reload);
 	});
 
 	// Watch Files For Changes
-	gulp.task('watch', function () {
-	 	gulp.watch(jsdirs, ['jshint']);
+	gulp.task('watch', function() {
+		gulp.watch(jsdirs, ['jshint']);
 	});
 
 
 	// Watch Files For Changes
-	gulp.task('testwatch', function () {
-	 	gulp.watch(specs, ['jasmine']);
+	gulp.task('testwatch', function() {
+		gulp.watch(specs, ['jasmine']);
 	});
 
-	gulp.task('jasmine', function () {
+	gulp.task('jasmine', function() {
 		return gulp.src([specs])
-		.pipe(debug({title: 'jasmine:'}))
-		.pipe(jasmine( {verbose: true} ));
+			.pipe(debug({
+				title: 'jasmine:'
+			}))
+			.pipe(jasmine({
+				verbose: true
+			}));
 	});
 
 
@@ -58,4 +66,3 @@
 	gulp.task('default', ['jshint', 'watch', 'servermonitor', 'browsermonitor', 'testwatch']);
 
 }());
-
