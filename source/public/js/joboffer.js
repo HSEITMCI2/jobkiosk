2a3a39803c1874a652e306fa786539cc25323d76
David Mändlen <david.maendlen@bifroe.st>
2015-12-11 12:50:30 +0100

    added company offer func

--- source/public/js/joboffer.js
+++ source/public/js/joboffer.js
@@ -9,24 +9,32 @@
 // 	var fields = ['familyname', 'givenname', 'email', 'company', 'department', 'street', 'postcode', 'city', 'countrytxt', 'password', 'resetPasswordToken', 'resetPasswordExpires'];
 
 // pseudo database
-var mockdata = [{ jobTitle: 'myfirsttitle' }, { jobTitle: 'mysecondtitle' }];
+var mockdata = [{ jobTitle: 'myfirsttitle' }, { jobTitle: 'mysecondtitle' }, { jobTitle: 'mythirdtitle' }];
+
+
 
 
 "use strict";
 
+
+
+
 function InitJoboffer() {
 
-
 	var that = {};
+
+
+
+
 	that.setup = function (jobData) {
 		for (var i = 0; i < jobData.length; i++) {
 
 			var job = jobData[i];
 
 			// create fields with dom
-			var input = dom('input', {name: 'jobTitle',class: 'jobInput'}, '');
-			var changeBtn = dom('button', {}, 'change');
-			var deleteBtn = dom('button', {}, 'delete');
+			var input = dom('input', { name: 'jobtitle', class: 'jobInput' }, '');
+			var changeBtn = dom('button', { class: 'jobchangebtn' }, 'change');
+			var deleteBtn = dom('button', { class: 'jobdeletebtn' }, 'delete');
 
 			// put everything into div
 			var jobContainer = dom('div', {}, input, changeBtn, deleteBtn);
@@ -35,20 +43,26 @@ function InitJoboffer() {
 			deleteBtn.jobIndex = i;
 			deleteBtn.addEventListener('click', function (e) {
 				console.log(e.target.jobIndex);
+
 				jobData.splice(e.target.jobIndex, 1);
 				removeChildren(jobContent);
 				that.setup(jobData);
 			});
 
 			// access jade / html id jobcontent
-			input.value = job.jobTitle;
+			input.value = job.jobtitle;
 			var jobContent = document.getElementById("jobcontent");
 			jobContent.appendChild(jobContainer);
 		}
-	}
-	return that;
+	};
+	that.updateFromServer = function () {
+		http('get', '/api/jobs', {}, function (responseText) {
+			response = JSON.parse(responseText);
+			that.setup(response);
+		});
+	};
 }
 
 window.addEventListener('load', function () {
-	InitJoboffer().setup(mockdata);
+	InitJoboffer().updateFromServer();
 });
